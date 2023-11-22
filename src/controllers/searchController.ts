f3e2c810503aabea0739ca8798983cc16fc52d92
GUILHERME TRAVASSOS OLIVEIRA PRADO DE GODOY <guilherme.topgodoy@senacsp.edu.br>
2023-11-22 17:11:59 -0300

    finalizando as telas do petshop

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,13 @@
 import {Request, Response} from 'express'
+import { Pets } from '../models/pet'
 
-export const search = (req:Request, res:Response) => {
-
+export const searchResult = (req:Request, res:Response) => {
+    let list = Pets.getFromName(req.body.name as string)
+    res.render('pages/pages',{
+        banner:{
+            title: 'Resultado da busca',
+            background: 'allanimals.jpg'
+        },
+        list
+    })
 }
